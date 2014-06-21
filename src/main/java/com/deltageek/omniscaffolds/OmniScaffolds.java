7e33582173171c4d96f9daee660104e24e41ce61
DeltaGeek <dchuhay+github@gmail.com>
2014-06-20 22:19:43 -0400

    Added mod base class

---
+++ src/main/java/com/deltageek/omniscaffolds/OmniScaffolds.java
@@ -0,0 +1,28 @@
+package com.deltageek.omniscaffolds;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "OmniScaffolds", name = "OmniScaffolds", version = "1.7.2-0.1")
+public class OmniScaffolds {
+
+    @Mod.Instance("OmniScaffolds")
+    public static OmniScaffolds instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
